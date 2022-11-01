4c1bf2f03144216a7caa55d15eef1f303aa1ef5b
Youssef-Elkady <elkadyyoussef23@gmail.com>
2022-11-01 17:11:56 +0200

    filling the class2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     // trying branching and pushing
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        class2 cs = new class2();
+        cs.PrintIt() ;
+      //  System.out.println("Hello world!");
     }
 }
 // 3rd change ok
--- src/class2.java
+++ src/class2.java
@@ -1,9 +1,9 @@
 // creating a third one with .java
 public class  class2 {
-    public static void class2
+    public class2() {
+    }
 
-    {
+    public void PrintIt() {
         System.out.println("class2!");
-
     }
 }
